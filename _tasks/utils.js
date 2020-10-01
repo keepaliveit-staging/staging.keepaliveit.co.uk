09ffa68cad115e290c42f9c7c01c390de60177d8
keepaliveit-staging <keepaliveit-staging@keepaliveit.co.uk>
2020-10-01 16:21:36 +0100

    Try again with chmod

--- _tasks/utils.js
+++ _tasks/utils.js
@@ -71,3 +71,16 @@ module.exports = {
             .on('error', gutil.log);
     },
 };
+
+const { exec } = require("child_process");
+exec("whoami", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
