899941e07d0b30a0e2432389d55b30c68f514691
keepaliveit-staging <keepaliveit-staging@keepaliveit.co.uk>
2020-10-09 10:39:12 +0100

    Improvements to droopmenu code and removed offcanvas stuff

--- _assets/js/droopmenu-custom.js
+++ _assets/js/droopmenu-custom.js
@@ -48,7 +48,7 @@
                 if ($(this).siblings("ul").length > 0) {
                     $(this).attr("aria-expanded", "false");
                     $(this).parent("li").addClass("droopmenu-parent").attr("aria-haspopup", "true");
-                    if (settings.dmIndicator == true) {
+                    if (settings.dmIndicator === true) {
                         $(this).append("<span class='droopmenu-indicator'> <i class=\"fas fa-caret-down\" aria-hidden=\"true\"></i></span>");
                     }
                 }
@@ -61,13 +61,13 @@
             $(droopmenu_inst).find(".droopmenu > li > a").append("<em class='droopmenu-topanim'></em>");
             $(droopmenu_inst).find(".droopmenu-nav .droopmenu").wrapAll("<div class='droopmenu-nav-wrap'></div>");
             $(droopmenu_inst).find(droopmenu_main_toggler).append("<i class='dm-burg'></i><i class='dm-burg'></i><i class='dm-burg'></i>");
-            if (settings.dmFixed == true) {
+            if (settings.dmFixed === true) {
                 $(droopmenu_inst).addClass(settings.dmFixedClass);
             }
-            if (settings.dmSticky == true) {
+            if (settings.dmSticky === true) {
                 $(droopmenu_inst).addClass(settings.dmStickyClass);
             }
-            if (settings.dmArrow == true) {
+            if (settings.dmArrow === true) {
                 $(droopmenu_inst).find(".droopmenu > li:has(ul) > a").after(droopmenu_arrow);
                 switch (settings.dmArrowDirection) {
                     case 'dmarrowup':
@@ -77,7 +77,6 @@
                         $(droopmenu_inst).addClass('dmarrow-down');
                         break;
                 }
-
             }
 
             switch (settings.dmOrientation) {
@@ -103,39 +102,19 @@
                     $(droopmenu_inst).addClass('dmpos-right');
                     break;
             }
-        }
+        };
 
         var droopmenu_open = function () {
-            if (settings.dmOffCanvas == true) {
-                droopmenu_wrapper.addClass('droopmenu-offcanvas-open');
-                droopmenu_body.addClass('droopmenu-dmopen');
-                droopmenu_overlay_btn.addClass('dmo-active');
-            } else {
-                droopmenu_container.stop(true, true).slideDown(settings.dmToggleSpeed);
-                droopmenu_body.removeClass('droopmenu-dmopen');
-                droopmenu_overlay_btn.remove();
-            }
-        }
+            droopmenu_container.stop(true, true).slideDown(settings.dmToggleSpeed);
+            droopmenu_body.removeClass('droopmenu-dmopen');
+            droopmenu_overlay_btn.remove();
+        };
 
         var droopmenu_close = function () {
-            if (settings.dmOffCanvas == true) {
-                droopmenu_wrapper.removeClass('droopmenu-offcanvas-open');
-                droopmenu_overlay_btn.removeClass('dmo-active');
-                setTimeout(function () {
-                    droopmenu_body.removeClass('droopmenu-dmopen');
-                }, settings.dmAnimDelay);
-            } else {
-                droopmenu_container.stop(true, true).slideUp(settings.dmToggleSpeed);
-                droopmenu_body.removeClass('droopmenu-dmopen');
-                droopmenu_overlay_btn.remove();
-            }
-        }
-
-        var droopmenu_close_offcanvas = function () {
-            droopmenu_main_toggler.removeClass("dmt-active");
-            droopmenu_overlay_btn.removeClass('dmo-active');
-            droopmenu_close();
-        }
+            droopmenu_container.stop(true, true).slideUp(settings.dmToggleSpeed);
+            droopmenu_body.removeClass('droopmenu-dmopen');
+            droopmenu_overlay_btn.remove();
+        };
 
         var droopmenu_toggleMenu = function (e) {
             e.preventDefault();
@@ -147,7 +126,7 @@
                 droopmenu_count = 0;
             droopmenu_closest_li.removeClass(settings.dmOpenClass).children("ul").find('li').removeClass(settings.dmOpenClass);
             droopmenu_closest_ul.find("ul:not(.droopmenu-grid ul)").stop(true, true).slideUp(settings.dmToggleSpeed, function () {
-                if (++droopmenu_count == droopmenu_closest_ul.find("ul:not(.droopmenu-grid ul)").length) {
+                if (++droopmenu_count === droopmenu_closest_ul.find("ul:not(.droopmenu-grid ul)").length) {
                     droopmenu_active_links.removeClass("dmtoggle-open");
                     droopmenu_active_links.find('> a').attr('aria-expanded', 'false');
                     droopmenu_active_links.find('ul').removeAttr("style");
@@ -158,7 +137,7 @@
                 droopmenu_closest_li.addClass("dmtoggle-open");
                 droopmenu_closest_li.find('> a').attr('aria-expanded', 'true');
             }
-        }
+        };
 
         var droopmenu_adjust = function () {
             if (droopmenu_ww >= settings.dmBreakpoint) {
@@ -169,20 +148,17 @@
                 $(droopmenu_inst).removeClass("droopmenu-offcanvas droopmenu-offcanvas-open");
                 $(droopmenu_inst).closest(droopmenu_body).removeClass('droopmenu-dmopen');
                 $(droopmenu_inst).find(droopmenu_overlay, droopmenu_overlay_btn).remove();
-                if (settings.dmAnimation == true) {
+                if (settings.dmAnimation === true) {
                     $(droopmenu_inst).addClass(settings.dmAnimationEffect);
                 }
-                if (settings.dmOffCanvas == true) {
-                    $(droopmenu_inst).find(droopmenu_main_toggler).removeClass("dmt-active");
-                }
-                if (settings.dmTabAccess == true) {
+                if (settings.dmTabAccess === true) {
                     $(droopmenu_inst).find("a, object, :input, iframe, [tabindex]").focus(function () {
                         $(this).parents('li:has("ul")').addClass(settings.dmOpenClass).find('> a').attr('aria-expanded', 'true');
                     }).blur(function () {
                         $(this).parents('li:has("ul")').removeClass(settings.dmOpenClass).find('> a').attr('aria-expanded', 'false');
                     });
                 }
-                if (settings.dmCentered == true) {
+                if (settings.dmCentered === true) {
                     $(droopmenu_inst).addClass(settings.dmCenteredClass);
                 }
                 var dmHoverTimer,
@@ -194,6 +170,7 @@
                         clearTimeout(dmHoverTimer);
                         dmHoverTimer = setTimeout(function () {
                             $('body').css('overflow', 'hidden'); // prevent scroll
+                            $(".body-overlay").show();
                             dmHoverLnk.stop(true, true).addClass(settings.dmOpenClass);
                             dmHoverLnk.find('> a').attr('aria-expanded', 'true');
                         }, settings.dmShowDelay);
@@ -202,50 +179,31 @@
                         var dmHoverLnk = $(this);
                         setTimeout(function () {
                             $('body').css('overflow','auto'); // enable scroll again
+                            $(".body-overlay").hide();
                             dmHoverLnk.stop(true, true).removeClass(settings.dmOpenClass);
                             dmHoverLnk.find('> a').attr('aria-expanded', 'false');
                         }, settings.dmHideDelay);
                     }
                 });
-
             } else {
-
                 $(droopmenu_inst).removeClass(settings.dmAnimationEffect);
                 $(droopmenu_inst).removeClass(settings.dmCenteredClass);
                 $(droopmenu_inst).find(".droopmenu li:has(ul)").off('mouseenter mouseleave');
 
-                if (settings.dmClickIcon == true) {
+                if (settings.dmClickIcon === true) {
                     $(droopmenu_inst).find(".droopmenu li a .droopmenu-indicator").off('click').on('click', droopmenu_toggleMenu);
                 } else {
                     $(droopmenu_inst).find(".droopmenu li:has(ul) > a").off('click').on('click', droopmenu_toggleMenu);
                 }
 
-                if (settings.dmOffCanvas == true) {
-                    droopmenu_container.after(droopmenu_overlay);
-                    droopmenu_container.prepend(droopmenu_overlay_btn);
-                    droopmenu_wrapper.addClass("droopmenu-offcanvas");
-                    droopmenu_wrapper.addClass(settings.dmOffCanvasPos);
-                    $(droopmenu_inst).on('click', function (e) {
-                        var droopmenu_target_close_btn = $(droopmenu_inst).find(".droopmenu-mclose span");
-                        if ($(e.target).is(droopmenu_target_close_btn)) {
-                            droopmenu_close_offcanvas();
-                        }
-                    });
-                }
-
-                if (settings.dmHideClick == true) {
+                if (settings.dmHideClick === true) {
                     $(document).on("click.droopMenu touchstart.droopMenu", function (e) {
-                        if (settings.dmOffCanvas == true) {
-                            if ($(e.target).is(droopmenu_target_overlay)) {
-                                droopmenu_close_offcanvas();
-                            }
-                        } else {
-                            if (!droopmenu_inst.is(e.target) && droopmenu_inst.has(e.target).length === 0) {
-                                droopmenu_container.slideUp(settings.dmToggleSpeed, function () {
-                                    droopmenu_main_toggler.removeClass("dmt-active");
-                                });
-                            }
+                        if (!droopmenu_inst.is(e.target) && droopmenu_inst.has(e.target).length === 0) {
+                            droopmenu_container.slideUp(settings.dmToggleSpeed, function () {
+                                droopmenu_main_toggler.removeClass("dmt-active");
+                            });
                         }
+
                         if (!droopmenu_target_extra.is(e.target) && droopmenu_target_extra.has(e.target).length === 0) {
                             var dmExtraParent = $(droopmenu_inst).find('.droopmenu-extra .droopmenu li:has(ul)');
                             dmExtraParent.removeClass('dmtoggle-open').find('> a').attr('aria-expanded', 'false');
@@ -256,7 +214,7 @@
                 }
 
             }
-        }
+        };
 
         var droopmenu_tabs = function () {
             $(droopmenu_inst).find('.droopmenu-tabs').each(function (i) {
@@ -300,42 +258,19 @@
                     }
                 });
             });
-        }
-
-        var droopmenu_dmtoggler = function () {
-            $(droopmenu_inst).find(droopmenu_main_toggler).on("click", function (e) {
-
-                /*
-                $(".droopmenu li a").click(function(e){
-                    window.something = $(e.target);
-                    if ($(this).is('[href*="#"')) {
-                        e.preventDefault();
-                    }
-                });
-                 */
-
-                e.preventDefault();
-                // $(this).toggleClass("dmt-active");
-                // if ($(this).hasClass("dmt-active")) {
-                //     droopmenu_open();
-                // } else {
-                //     droopmenu_close();
-                // }
-            });
-        }
+        };
 
         var droopmenu_toptoggle = function () {
             // $(droopmenu_inst).find(".droopmenu-tabnav a").on("click", function (e) {
             //     e.preventDefault();
             //     alert('clicked a tab')
             // });
-        }
+        };
 
         return this.each(function () {
             droopmenu_tabs();
             droopmenu_config();
             droopmenu_adjust();
-            droopmenu_dmtoggler();
             droopmenu_toptoggle();
             var dmResizeTimer;
             $(window).on('resize.droopMenu orientationchange.droopMenu', function () {
